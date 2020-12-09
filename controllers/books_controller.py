b2c6d340aaae0abcd7e6e0a8ef26e6918dfb21d8
Andrew Wood <andrewwood90@hotmail.com>
2020-12-09 15:03:45 +0000

    initial commit

---
+++ controllers/books_controller.py
@@ -0,0 +1,19 @@
+
+# NEW
+# GET '/books/new'
+
+# CREATE
+# POST '/books'
+
+# SHOW
+# GET '/books/<id>'
+
+# EDIT
+# GET '/books/<id>/edit'
+
+# UPDATE
+# PUT '/books/<id>'
+
+# DELETE
+# DELETE '/books/<id>'
+
